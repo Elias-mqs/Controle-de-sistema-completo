03cdf3e540c18ca40aa4f206c8784d80526357fb
Elias-mqs <eliasmqs397@gmail.com>
2024-04-09 17:51:40 -0400

    verificar arquivo auth

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,9 +1,21 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-let: PrismaClient;
+
+let prisma: PrismaClient;
 
 if(process.env.NODE_ENV === 'production'){
    prisma = new PrismaClient(); 
 }else{
-    let globalWithPrisma
+    let globalWithPrisma = global as typeof globalThis & {
+        prisma: PrismaClient;
+    }
+
+    if(!globalWithPrisma.prisma){
+        globalWithPrisma.prisma = new PrismaClient();
+    }
+
+prisma = globalWithPrisma.prisma;
+
 }
+
+export default prisma;
